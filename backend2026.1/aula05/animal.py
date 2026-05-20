a0f2cfd64cdb2e64cce93ab38f6a35e0240ce773
VictorSacramento <victorscosta@gmail.com>
2026-05-20 18:49:15 -0300

    aula05

---
+++ backend2026.1/aula05/animal.py
@@ -0,0 +1,6 @@
+class Animal:
+    def __init__(self,nome_animal, idade_animal, raca_animal, tipo_animal):
+        self.nome = nome_animal
+        self.idade = idade_animal
+        self.raca = raca_animal
+        self.tipo = tipo_animal
