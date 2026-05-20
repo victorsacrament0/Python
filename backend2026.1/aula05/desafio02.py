d11c472fe923bf5972e1ecb4223dbe9d38d62f7d
VictorSacramento <victorscosta@gmail.com>
2026-05-20 20:53:59 -0300

    aula05

--- backend2026.1/aula05/desafio02.py
+++ backend2026.1/aula05/desafio02.py
@@ -42,3 +42,32 @@ conta.realizar_deposito()
 conta.saldo_atual()
 
 """
+
+
+""" terceira ideia
+
+class ContaBancaria:
+    def __init__(self):
+        self.saldo =  0        
+ 
+    def total_saldo(self):
+        mensagem = f"O saldo atual é de R$ {self.saldo}"
+        return mensagem
+   
+    def sobre_deposito(self):
+        valor = int(input("Informe o valor a depositar: R$"))
+ 
+        if valor > 0:
+            self.saldo += valor
+            print(f"Deposito de R${valor:.2f} foi feito")  
+        else:
+            print("Não foi possivel fazer esse depósito")
+ 
+cliente = ContaBancaria()
+ 
+print(f"Antes do deposito {cliente.total_saldo()}")
+cliente.sobre_deposito()
+ 
+print(cliente.total_saldo())
+
+ """
