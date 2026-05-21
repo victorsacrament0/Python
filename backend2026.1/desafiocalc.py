cc3329c2c5f4dbf561d697f8f1578ebbead9dc7c
VictorSacramento <victorscosta@gmail.com>
2026-05-21 12:56:35 -0300

    cmmt

--- backend2026.1/desafiocalc.py
+++ backend2026.1/desafiocalc.py
@@ -1,31 +1,47 @@
 class Calculadora:
-    def __init__(self,number1,number2):
-        self.nu1 = number1
-        self.nu2 = number2
+    def __init__(self):
+        print('|:.Calculadora Python.:|')
 
-    def somar(self):
-        mensagem = f"\nResultado: {self.nu1} + {self.nu2} = {self.nu1 + self.nu2}\n"
-        return mensagem 
+    def somar (self,nu1,nu2):
+        print (f"\n|:Resultado: {nu1} + {nu2} = {nu1 + nu2}:|\n")
     
-    def subtrair (self):
-        mensagem = f"\nResultado: {self.nu1} - {self.nu2} = {self.nu1 - self.nu2}\n"
-        return mensagem 
+    def subtrair (self,nu1,nu2):
+        print(f"\n|:Resultado: {nu1} - {nu2} = {nu1 - nu2}:|\n")
     
-    def dividir (self):
-        mensagem = f"\nResultado: {self.nu1} ÷ {self.nu2} = {self.nu1 / self.nu2}\n"
-        return mensagem 
+    def dividir (self,nu1,nu2):
+        print(f"\n|:Resultado: {nu1} ÷ {nu2} = {nu1 / nu2}:|\n")
 
-    def multiplicar (self):
-        mensagem = f"\nResultado: {self.nu1} x {self.nu2} = {self.nu1 * self.nu2}\n"
-        return mensagem
+    def multiplicar (self,nu1,nu2):
+        print(f"\n|:Resultado: {nu1} x {nu2} = {nu1 * nu2}:|\n")
+        
 
     def main(self):
+        
         while True:
-            
+            opcao = int(input("Escolha um opção:(Para sair, digitar qualquer outro valor)\n[1 - Somar]\n[2 - Subtrair]\n[3 - Dividir]\n[4 - Multiplicar]\n ->"))
+ 
+            if opcao == 1:
+                number1 = float(input('Digite o primeiro número: '))
+                number2 = float(input('Digite o segundo número: '))
+                self.somar(number1,number2)
+            elif opcao == 2:
+                number1 = float(input('Digite o primeiro número: '))
+                number2 = float(input('Digite o segundo número: '))
+                self.subtrair(number1,number2)
+            elif opcao == 3:
+                number1 = float(input('Digite o primeiro número: '))
+                number2 = float(input('Digite o segundo número: '))
+                self.dividir(number1,number2)
+            elif opcao == 4:
+                number1 = float(input('Digite o primeiro número: '))
+                number2 = float(input('Digite o segundo número: '))
+                self.multiplicar(number1,number2)
+            else:
+                print(f"\n| VALOR INVALIDO |\n")
+                break
 
 
 
 
-numero = Calculadora(4,2)
-numero.somar()
-print(numero.dividir())
+numero = Calculadora()
+numero.main()
