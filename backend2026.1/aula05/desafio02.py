bc06dd6a51fe8d9f9b8ec620af957a89f4700631
VictorSacramento <victorscosta@gmail.com>
2026-05-20 21:32:00 -0300

    aula05

--- backend2026.1/aula05/desafio02.py
+++ backend2026.1/aula05/desafio02.py
@@ -1,25 +1,4 @@
 class ContaBancaria:
-    def __init__(self,saldo,deposito):
-        self.saldob = saldo
-        self.depo = deposito
-
-    def info_saldo(self):
-        print(f'O saldo é de  R${self.saldob}')
-
-    def info_dep(self):
-        if self.depo > 0:
-            print(f'O deposito foi de R${self.depo}')
-        else:
-            print(f'Apenas depositos de valores positivos')
-
-operacao = ContaBancaria(1000,500)
-operacao.info_saldo()
-operacao.info_dep()
-
-
-""" outra forma  
-
-class ContaBancaria:
     def __init__(self):
         self.__saldo = 0
    
@@ -40,7 +19,7 @@ conta.saldo_atual()
 conta.realizar_deposito()
 conta.saldo_atual()
 
-"""
+
 
 
 """ terceira ideia
