87471928aa0b81861cbf9945d0dd3efe83410d9c
VictorSacramento <victorscosta@gmail.com>
2026-05-29 20:39:00 -0300

    aula09

--- XPTO/cadastro/views.py
+++ XPTO/cadastro/views.py
@@ -10,6 +10,7 @@ def dados_alunos (request):
         {'nome' : 'Thyago Matarazzo','curso' : 'Análise e Desenvolvimento de Sistemas','turma' : '2026.2M'},
         {'nome' : 'João Gutierrez','curso' : 'Ciência da Computação','turma' : '2026.1T'},
         {'nome' : 'Marcela Correa','curso' : 'Engenharia de Software','turma': '2026.1N'},
+        {'nome' : 'Pedro Pereira','curso' : 'Sistemas de Informação','turma': '2026.2N'},
     ]
 
     return render(request, 'alunos\dados_alunos.html', { 'mensagem':titulo, 'clientesdb':lista_alunos })
