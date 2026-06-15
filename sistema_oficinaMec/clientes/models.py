1c03df5edab983eabbc1b24711b0129eab3eced0
VictorSacramento <victorscosta@gmail.com>
2026-06-15 16:33:56 -0300

    sysmec

--- sistema_oficinaMec/clientes/models.py
+++ sistema_oficinaMec/clientes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=55)
+    sobrenome = models.CharField(max_length=60)
+    email = models.EmailField(max_length=150)
+    cpf = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.nome
+    
+class Carro(models.Model):
+    carro = models.CharField(max_length=255)
+    placa = models.CharField(max_length=10)
+    ano = models.IntegerField() 
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    lavagens = models.IntegerField(default=0)
+    consertos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.carro
