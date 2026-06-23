faf2d4247521404ae2fd4c648edc8b42eddcbb12
VictorSacramento <victorscosta@gmail.com>
2026-06-23 11:15:16 -0300

    sysmec

--- sistema_oficinaMec/servicos/views.py
+++ sistema_oficinaMec/servicos/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .models import Servico
 from .forms import FormServico
+from fpdf import FPDF
+from io import BytesIO
 
 
 
@@ -26,3 +28,38 @@ def listar_servico(request):
 def servico(request, identificador):
     servico = get_object_or_404(Servico, identificador=identificador)
     return render(request, 'servico.html', {'servico':servico})
+
+def gerar_os (gerar_os, identificador):
+    servico = get_object_or_404 (Servico, identificador=identificador)
+    
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 12)
+    pdf.set_fill_color(240,240,240)
+    pdf.cell(35, 10,' Cliente:',1 ,0,'L', 1)
+    pdf.cell(0, 10,f' {servico.cliente.nome}',1 ,1 ,'L', 1)
+    
+    pdf.cell(35, 10,'Manutenção:',1 ,0,'L', 1)
+    categorias_manutencao = servico.categoria_manutencao.all()
+    for i , manutencao in enumerate(categorias_manutencao):
+        pdf.cell(0, 10,f' - {manutencao.get_titulo_display()}',1 ,1,'L', 1)
+        if not i == len(categorias_manutencao) -1:
+            pdf.cell(35, 10,'',0 ,0)
+
+    pdf.cell(35, 10,'Início:',1 ,0,'L', 1)
+    pdf.cell(0, 10,f' {servico.data_inicio}',1 ,1 ,'L', 1)
+
+    pdf.cell(35, 10,'Prev. término:',1 ,0,'L', 1)
+    pdf.cell(0, 10,f' {servico.data_entrega}',1 ,1 ,'L', 1)
+
+    pdf.cell(35, 10,'Valor:',1 ,0,'L', 1)
+    pdf.cell(0, 10,f' R${servico.preco_total():.2f}',1 ,1 ,'L', 1)
+
+    pdf.cell(35, 10,'Protocolo:',1 ,0,'L', 1)
+    pdf.cell(0, 10,f' {servico.protocolo}',1 ,1 ,'L', 1)
+
+    pdf_content = pdf.output(dest='S').encode('latin1')
+    pdf_bits = BytesIO(pdf_content)
+    return FileResponse(pdf_bits, filename=f'os-{servico.protocolo}.pdf')
+    
+    
