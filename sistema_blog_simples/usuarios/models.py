397f7328f8ccfbf3ff64f61665a4e94552793e97
VictorSacramento <victorscosta@gmail.com>
2026-06-27 23:04:24 -0300

    sysblog

--- sistema_blog_simples/usuarios/models.py
+++ sistema_blog_simples/usuarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Perfil(models.Model):
     user  = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,3 +10,11 @@ class Perfil(models.Model):
 
     def __str__(self):
         return f'Perfil de {self.user.username}'
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)                      #diminuindo o tamanho das imagens para salvar, pra ficar mais leve
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
