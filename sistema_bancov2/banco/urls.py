7660c200c15436bd9bf3639f570c254561624734
VictorSacramento <victorscosta@gmail.com>
2026-07-13 14:00:13 -0300

    bankv2

--- sistema_bancov2/banco/urls.py
+++ sistema_bancov2/banco/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,3 +12,5 @@ urlpatterns = [
     path('acessar_conta/', views.acessar_conta, name='acessar_conta'),
     path('sair/', views.sair, name='sair')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
